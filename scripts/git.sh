7dc27221dd58ca9eebdcb661613fdd001e9bae43
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2026-06-27 14:05:59 -0400

    Add scripts/git.sh

---
+++ scripts/git.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+BASEDIR=`dirname -- "$0"` || exit $?
+BASEDIR=`realpath -- "${BASEDIR}"` || exit $?
+
+main()
+{
+    local project
+    project="$1"
+
+    if [ $# -gt 0 ]; then
+        shift
+    fi
+
+    if [ -z "${project}" ]; then
+        local errlevel=0
+
+        for project in "${BASEDIR}/../projects"/*; do
+            project="${project##*/}"
+
+            main "${project}" "$@"
+
+            if [ ${errlevel} -eq 0 ]; then
+                errlevel=$?
+            fi
+        done
+        exit ${errlevel}
+    fi
+
+    local wrkdir
+    wrkdir="${BASEDIR}/../wrkdir"
+
+    local wrksrc
+    wrksrc="${wrkdir}/${project}"
+
+    git -C "${wrksrc}" "$@"
+
+    return $?
+}
+
+main "$@"
